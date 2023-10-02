51c99ebb54581942f4888211abb079440e93eb37
Pablo <pablocg7518@gmail.com>
2023-10-02 09:43:43 +0200

    Agregados años y fecha de nacimiento

--- prueba.java
+++ prueba.java
@@ -3,6 +3,7 @@ public class prueba {
 		public static void main(String args[]){
 	
 			System.out.println("Pablo Caballero Gonzalez");
+			System.out.println("19 anos, 25 de marzo de 2004");
 	
 		}
 	
