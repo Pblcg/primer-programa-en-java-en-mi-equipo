86061be087a738f1555e51e1c876dac1ecd2a20b
Pablo <pablocg7518@gmail.com>
2023-10-02 09:49:01 +0200

    Agregada mi comida favorita

--- prueba.java
+++ prueba.java
@@ -4,6 +4,7 @@ public class prueba {
 	
 			System.out.println("Pablo Caballero Gonzalez");
 			System.out.println("19 anos, 25 de marzo de 2004");
+			System.out.println("La pizza es mi comida favorita");
 	
 		}
 	
