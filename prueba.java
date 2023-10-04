d80f87bd2fc3a9d46e55455212ac078d7fc88f93
Pablo <pablocg7518@gmail.com>
2023-10-04 14:03:06 +0200

    Elimininados mi nombre y apellidos y agregado mi grupo de música o cantante favorito

--- prueba.java
+++ prueba.java
@@ -2,9 +2,9 @@ public class prueba {
 	
 		public static void main(String args[]){
 	
-			System.out.println("Pablo Caballero Gonzalez");
 			System.out.println("19 anos, 25 de marzo de 2004");
 			System.out.println("La pizza es mi comida favorita");
+			System.out.println("No tengo grupo de música o cantante favorito");
 	
 		}
 	
